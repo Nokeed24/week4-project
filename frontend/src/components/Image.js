a093b711b1b10dcfced724ed2f56eac352bdce2b
ludmilla <l.carlier@live.nl>
2018-05-14 14:44:15 +0200

    setting up folders

---
+++ frontend/src/components/Image.js
@@ -0,0 +1,12 @@
+class Image extends PureComponent {
+    static propTypes = {
+        RandomDog: PropTypes.func.isRequired
+    }
+
+        render() {
+            <div>
+
+            </div>
+        }
+
+    }
