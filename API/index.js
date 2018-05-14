9c3a1bb87867d2060d3b5847a4befaf072123a5d
emilhellblom <ehellblom@outlook.com>
2018-05-14 17:32:42 +0200

    minor change

--- API/index.js
+++ API/index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser')
 const { Client } = require('pg')
 const likesRouter = require('./likes/router')
 const usersRouter = require('./users/router')
-const bodyParser = require('body-parser')
 const User = require('./users/model')
 
 const app = express()
@@ -18,13 +17,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
     next()
-<<<<<<< HEAD
 })
-=======
-  })
-
-
->>>>>>> Userspost
 
 const connectionString = 'postgresql://postgres:password@localhost:5432/postgres'
 const client = new Client({ connectionString })
