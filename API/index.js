172f9136b60554ed0e69108841b71d1e98e6af3d
Nokeed24 <nokid_gr@hotmail.it>
2018-05-14 16:30:51 +0200

    can now POST a like

--- API/index.js
+++ API/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const { Client } = require('pg')
 const likesRouter = require('./likes/router')
 
 const app = express()
+app.use(bodyParser.json())
 
 var Sequelize = require('sequelize')
 var sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
@@ -14,11 +16,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
     next()
-  })
-
-
-// app.use(usersRouter)
-
+})
 
 const connectionString = 'postgresql://postgres:password@localhost:5432/postgres'
 const client = new Client({ connectionString })
--- API/likes/router.js
+++ API/likes/router.js
@@ -27,5 +27,30 @@ router.get('/likes', (req, res) => {
       })
 })
 
+router.get('/likes/:id', (req, res) => {
+  const id = req.params.id
+	Like.findById(id)
+	  .then(result => {
+	  	if (!result) {
+	  		res.status(404).send({error: 'Does not exist'})
+	  	}
+	  	else {
+	  		res.send(result)
+	  	}
+	  })
+	  .catch(err => {
+	    res.status(500).send({error: 'Something went wrong with Postgres'})
+	  })
+})
+
+//router.post('/likes', requireUser, (req, res) => {
+router.post('/likes', (req, res) => {
+  const like = req.body
+
+  Like.create(like).then(entity => {
+    res.status(201).send(entity)
+  })
+})
+
 
 module.exports = router
