8a596b28d7b1b93bc23dfdb502b6732eb21ed441
emilhellblom <ehellblom@outlook.com>
2018-05-16 14:16:13 +0200

    Gets the id

--- frontend/src/reducers/currentUser.js
+++ frontend/src/reducers/currentUser.js
@@ -1,9 +1,10 @@
 import {USER_LOGIN_SUCCESS} from '../actions/users'
 
 export default function (state = null, action) {
+  console.log(action.payload, "CURRENTUSER 12")
  	switch (action.type) {
 	 	case USER_LOGIN_SUCCESS:
-    console.log(action.payload, "CURRENTUSER 12")
+
 			 return action.payload
 
 
