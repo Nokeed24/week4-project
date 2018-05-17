9067efaf448de3ee8b91970241799756e2e0236c
emilhellblom <ehellblom@outlook.com>
2018-05-17 17:40:34 +0200

    update match algorithm

--- frontend/src/lib/functions.js
+++ frontend/src/lib/functions.js
@@ -21,10 +21,16 @@ const removeSubBreed = (breedName) => {
 
 export const matchUsers = (allusers, currentUser, likes, allbreeds) => {
   console.log('All users in functions', allusers, currentUser)
+  console.log('All breeds in functions', allbreeds)
   const likesMatches = allusers.filter(users =>
     users.likes === currentUser.likes)
   console.log(likesMatches, 'Matches by likes')
-  // const breedsOfMatch = allbreeds.filter(likes =>
-  //   likes.id === likesMatches.breed)
-  // console.log(breedMatches, 'Matches by breed')
+  // const breedsOfMatch = allbreeds.filter(breed => {
+  //   console.log(allbreeds.indexOf(breed))
+  //   if(allbreeds.indexOf(breed) < likesMatches.length)
+  //   {return breed.id === likesMatches[allbreeds.indexOf(breed)].id}})
+  //   console.log(breedsOfMatch, 'Matches by breed')
+  // const commonLikes = likes.filter(breed =>
+  //   breed.breed === breedsOfMatch[likes.indexOf(breed)].breed)
+  //   console.log(commonLikes, 'COMMON likes')
 }
