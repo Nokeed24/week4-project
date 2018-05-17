10db7381c217cc2b86f1824cf4fa31b39ce61008
Unknown <ehellblom@outlook.com>
2018-05-17 10:58:37 +0200

    Added temporary fix for like button

--- frontend/src/components/LikeButton.js
+++ frontend/src/components/LikeButton.js
@@ -12,23 +12,32 @@ export class LikeButton extends PureComponent {
     const {breed, fetchRandom, fetchLikesForId, currentUser, postLike, likes, updateLike} = this.props
     const newLike = {id: currentUser.id, breed: breed }
     postLike(newLike)
-    fetchLikesForId(currentUser.id)
-    console.log("Number of likes", likes.length )
+    // fetchLikesForId(currentUser.id)
+    // console.log("Number of likes", likes.length )
 
-    const amountOfLikes = {
-      likes: likes.length
-    }
-
-    updateLike(amountOfLikes)
     //const numberoflikes = fetchLikesForId(currentUser.id)
     //console.log("Number of likes", numberoflikes.length)
     fetchRandom()
   }
 
+  componentDidUpdate(prevProps){
+    const {breed, fetchLikesForId, currentUser, likes, updateLike} = this.props
+    console.log(prevProps, 'previous');
+    console.log(this.props, 'new props');
+    if (prevProps.breed !== breed && likes!==null) {
+      fetchLikesForId(currentUser.id)
+      const amountOfLikes = {
+        likes: likes.length
+      }
+
+      updateLike(amountOfLikes)
+    }
+  }
+
   render() {
     return (
       <button className="like like__yes" onClick={this.onClick}>
-        <i class="fas fa-check-circle fa-5x"></i>
+        <i className="fas fa-check-circle fa-5x"></i>
       </button>
     )
   }
@@ -41,5 +50,6 @@ const mapStateToProps = ({breed, url, currentUser, likes, updateLike }) => ({
   currentUser,
   updateLike
 })
+
 
 export default connect(mapStateToProps, { fetchRandom, fetchLikesForId, postLike, updateLike })(LikeButton)
