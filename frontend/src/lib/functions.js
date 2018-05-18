68fab750843de166fd02e54bf6cc70838bd82a2b
emilhellblom <ehellblom@outlook.com>
2018-05-18 10:52:19 +0200

    functions attempt

--- frontend/src/lib/functions.js
+++ frontend/src/lib/functions.js
@@ -19,18 +19,43 @@ const removeSubBreed = (breedName) => {
 
 }
 
+// const allLikes = [
+//   { id: 1, name: 'no' },
+//   { id: 1, name: 'lab' },
+//   { id: 2, name: 'doggo3' },
+//   { id: 2, name: 'labrador' },
+//   { id: 3, name: 'doggo3' },
+//   { id: 4, name: 'doggo3' },
+//   { id: 5, name: 'labrador' },
+//   { id: 6, name: 'doggo3' }
+// ]
+//
+// const allUsers = [
+//   { id: 1, likes: 40 },
+//   { id: 2, likes: 40 },
+//   { id: 3, likes: 56 },
+//   { id: 4, likes: 11 },
+//   { id: 5, likes: 40 },
+//   { id: 6, likes: 40 },
+// ]
+//
+// const currentUser = { id: 1, likes: 40, dog: 'labrador' };
+
+
 export const matchUsers = (allusers, currentUser, likes, allbreeds) => {
-  console.log('All users in functions', allusers, currentUser)
-  console.log('All breeds in functions', allbreeds)
-  const likesMatches = allusers.filter(users =>
-    users.likes === currentUser.likes)
-  console.log(likesMatches, 'Matches by likes')
-  // const breedsOfMatch = allbreeds.filter(breed => {
-  //   console.log(allbreeds.indexOf(breed))
-  //   if(allbreeds.indexOf(breed) < likesMatches.length)
-  //   {return breed.id === likesMatches[allbreeds.indexOf(breed)].id}})
-  //   console.log(breedsOfMatch, 'Matches by breed')
-  // const commonLikes = likes.filter(breed =>
-  //   breed.breed === breedsOfMatch[likes.indexOf(breed)].breed)
-  //   console.log(commonLikes, 'COMMON likes')
+  console.log(allusers, currentUser, likes, allbreeds, 'All parameters')
+  let userMatches = [];
+  allusers.map(user => user.likes === currentUser.likes ? userMatches.push(user) : null)
+  console.log('Users match by amount of likes', userMatches.map(x => x.id))
+  let random = []
+  allbreeds.map(x => random.push(x.breed))
+  // console.log(likes.map(x => random.filter(y => y === x.breed)))
+  // let random3 = random.map(x => x.include(random2[x.indexOf()]))
+  // console.log(random3, 'MATCHESFFFSS   SJNFLKSN')
+  const matchingUsers =
+    userMatches.map(match => allbreeds.filter(y => y.id === match.id))
+  console.log(matchingUsers, 'This is a variable with two arrays with users that have matched in amount of likes')
+  
+  // matchingUsers.map((x, i) => x[i].concat(x[i+1]))
+  console.log('breedsofmatch', matchingUsers.map((x, i) => x[i].concat(x[i+1])))
 }
