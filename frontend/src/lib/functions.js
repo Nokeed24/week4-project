cae6c9d70c77699de82f302ee9adf5cfeb8a49cd
Unknown <nokid_gr@hotmail.it>
2018-05-18 11:49:00 +0200

    Common likes works
    CommonLikes works as intended

--- frontend/src/lib/functions.js
+++ frontend/src/lib/functions.js
@@ -19,43 +19,47 @@ const removeSubBreed = (breedName) => {
 
 }
 
-// const allLikes = [
-//   { id: 1, name: 'no' },
-//   { id: 1, name: 'lab' },
-//   { id: 2, name: 'doggo3' },
-//   { id: 2, name: 'labrador' },
-//   { id: 3, name: 'doggo3' },
-//   { id: 4, name: 'doggo3' },
-//   { id: 5, name: 'labrador' },
-//   { id: 6, name: 'doggo3' }
-// ]
-//
-// const allUsers = [
-//   { id: 1, likes: 40 },
-//   { id: 2, likes: 40 },
-//   { id: 3, likes: 56 },
-//   { id: 4, likes: 11 },
-//   { id: 5, likes: 40 },
-//   { id: 6, likes: 40 },
-// ]
-//
-// const currentUser = { id: 1, likes: 40, dog: 'labrador' };
+const removeDuplicates = (arr, key) => {
+    if (!(arr instanceof Array) || key && typeof key !== 'string') {
+        return false;
+    }
 
+    if (key && typeof key === 'string') {
+        return arr.filter((obj, index, arr) => {
+            return arr.map(mapObj => mapObj[key]).indexOf(obj[key]) === index;
+        });
+
+    } else {
+        return arr.filter(function(item, index, arr) {
+            return arr.indexOf(item) == index;
+        });
+    }
+}
+//THIS WILL TAKE TWO LISTS OF USER LIKES AND RETURN THE COMMON ONES
+const returnCommonLikes = (userLikes, myLikes) => {
+    var common = []
+    userLikes.forEach(like => {
+        myLikes.forEach(myLike => {
+            if(like.breed === myLike.breed)
+            {
+                common.push(like.breed)
+            }
+        })
+    })
+    return common
+}
 
 export const matchUsers = (allusers, currentUser, likes, allbreeds) => {
   console.log(allusers, currentUser, likes, allbreeds, 'All parameters')
   let userMatches = [];
   allusers.map(user => user.likes === currentUser.likes ? userMatches.push(user) : null)
-  console.log('Users match by amount of likes', userMatches.map(x => x.id))
+  console.log('Users match by amount of likes', userMatches)
   let random = []
   allbreeds.map(x => random.push(x.breed))
-  // console.log(likes.map(x => random.filter(y => y === x.breed)))
-  // let random3 = random.map(x => x.include(random2[x.indexOf()]))
-  // console.log(random3, 'MATCHESFFFSS   SJNFLKSN')
   const matchingUsers =
     userMatches.map(match => allbreeds.filter(y => y.id === match.id))
   console.log(matchingUsers, 'This is a variable with two arrays with users that have matched in amount of likes')
-  
-  // matchingUsers.map((x, i) => x[i].concat(x[i+1]))
-  console.log('breedsofmatch', matchingUsers.map((x, i) => x[i].concat(x[i+1])))
+  const common = returnCommonLikes(matchingUsers[0],removeDuplicates(matchingUsers[1])) 
+    
+  console.log(removeDuplicates(common), "common likes")
 }
